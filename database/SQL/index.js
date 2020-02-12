3d27069cc258ade106033da91dae14318460100a
naszzz <naszagorov@gmail.com>
2020-02-12 14:21:13 -0800

    crud placeholders

---
+++ database/SQL/index.js
@@ -0,0 +1,19 @@
+// create database connection
+// write database querying functions
+// export functions to be used in server api
+
+
+// const createData = function() {};
+
+// const getData = function() {};
+
+// const updateData = function () {};
+
+// const deleteData = function() {};
+
+// module.exports = {
+//   createData: createData,
+//   getData: getData,
+//   updateData: updataData,
+//   deleteData: deleteData,
+// }
