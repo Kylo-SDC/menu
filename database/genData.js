436d5f5087bb64318705a6ed9831e1b518df25e8
naszzz <naszagorov@gmail.com>
2020-02-13 15:56:19 -0800

    make genData file

---
+++ database/genData.js
@@ -0,0 +1,52 @@
+
+const faker = require('faker');
+// Helper Functions
+const randomItems = () => Math.floor(Math.random() * 6) + 4;
+const randomSections = () => Math.floor(Math.random() * 3) + 1;
+
+// create 100 records
+const records = [];
+const generateData = function() {
+  for (let i = 0; i < 100; i += 1) {
+    records[i] = {
+      id: i + 1,
+      restaurant: faker.random.words(),
+      menus: [],
+    };
+    for (let j = 0; j < 5; j += 1) {
+      records[i].menus[j] = {
+        title: faker.random.word(),
+        description:
+          `${faker.lorem.sentence()}
+          ${faker.lorem.sentence()}
+          ${faker.lorem.sentence()}
+          ${faker.lorem.sentence()}
+          ${faker.lorem.sentence()}
+          ${faker.lorem.sentence()}
+          ${faker.lorem.sentence()}`,
+        sections: [],
+      };
+      const sectionLength = randomSections();
+      for (let k = 0; k < sectionLength; k += 1) {
+        records[i].menus[j].sections[k] = {
+          title: faker.random.words(),
+          items: [],
+        };
+        const itemLength = randomItems();
+        for (let l = 0; l < itemLength; l += 1) {
+          records[i].menus[j].sections[k].items[l] = {
+            title: faker.random.words(),
+            price: `$${faker.finance.amount()}`,
+            description:
+              `${faker.lorem.sentence()}
+              ${faker.lorem.sentence()}
+              ${faker.lorem.sentence()}`,
+          };
+        }
+      }
+    }
+  }
+  return records;
+};
+
+module.exports = generateData;
--- database/seed.js
+++ database/seed.js
@@ -1,5 +1,5 @@
 const process = require('process');
-const faker = require('faker');
+const generateData = require('./genData.js');
 const mongoose = require('mongoose');
 const restaurantSchema = require('./schema.js');
 
@@ -10,57 +10,8 @@ mongoose.connect('mongodb://localhost/menus', {
   useFindAndModify: false,
 });
 
-// Helper Functions
-const randomItems = () => Math.floor(Math.random() * 6) + 4;
-const randomSections = () => Math.floor(Math.random() * 3) + 1;
-
-// create 100 records
-const records = [];
-for (let i = 0; i < 100; i += 1) {
-  records[i] = {
-    id: i + 1,
-    restaurant: faker.random.words(),
-    menus: [],
-  };
-
-  for (let j = 0; j < 5; j += 1) {
-    records[i].menus[j] = {
-      title: faker.random.word(),
-      description:
-        `${faker.lorem.sentence()}
-         ${faker.lorem.sentence()}
-         ${faker.lorem.sentence()}
-         ${faker.lorem.sentence()}
-         ${faker.lorem.sentence()}
-         ${faker.lorem.sentence()}
-         ${faker.lorem.sentence()}`,
-      sections: [],
-    };
-
-    const sectionLength = randomSections();
-    for (let k = 0; k < sectionLength; k += 1) {
-      records[i].menus[j].sections[k] = {
-        title: faker.random.words(),
-        items: [],
-      };
-
-      const itemLength = randomItems();
-      for (let l = 0; l < itemLength; l += 1) {
-        records[i].menus[j].sections[k].items[l] = {
-          title: faker.random.words(),
-          price: `$${faker.finance.amount()}`,
-          description:
-            `${faker.lorem.sentence()}
-             ${faker.lorem.sentence()}
-             ${faker.lorem.sentence()}`,
-        };
-      }
-    }
-  }
-}
-
+const records = generateData();
 const Restaurants = mongoose.model('restaurants', restaurantSchema);
-
 // remove all records and add all records in the array
 Restaurants.remove({}, (err1) => {
   if (err1) {
