83c5a4efa4d9a47eace03e64805294221bf6a9d0
naszzz <naszagorov@gmail.com>
2020-02-21 15:05:45 -0800

    got rid of console log

--- server/index.js
+++ server/index.js
@@ -42,6 +42,7 @@ app.post('/api/menu/:restaurantId', (req, res) => {
   let { restaurantId } = req.params;
   restaurantId = Number(restaurantId);
   const { menuName, menuDescription } = req.body;
+  console.log(menuName, menuDescription);
   db.createMenu(restaurantId, menuName, menuDescription, (err, result) => {
     if (err) {
       res.status(400).json(err);
